fedff5713c54e5e791ad660c5f1d841b01ac91a2
MiguelP741 <miguelaraujo.fox05@gmail.com>
2025-03-11 15:17:18 -0300

    Inserção de nome

---
+++ qualNome.js
@@ -0,0 +1,5 @@
+let nomeUsuario = prompt("Olá, me diga seu nome: ");
+
+// nomeUsuario - o Usuário irá colocar o nome que corresponderá a essa variável (nomeUsuario)
+
+console.log("Bem vindo ao site: "+nomeUsuario);
